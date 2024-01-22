6aa51398d7409dda9589621a2cab5dfac5f75e53
root <root@ip-10-72-9-201.ap-south-2.compute.internal>
2024-01-22 11:20:53 +0000

    m

--- tests/cxr/fhir_validation/test_fhir.py
+++ tests/cxr/fhir_validation/test_fhir.py
@@ -3,7 +3,7 @@ from marinaalchemist import BaseClass
 import allure
 import re
 
-@pytest.mark.usefixtures("container_auto")
+# @pytest.mark.usefixtures("container_auto")
 class TestFHIR(BaseClass):
     
 
@@ -20,12 +20,12 @@ class TestFHIR(BaseClass):
     '''
     def test_fhir_obs_code(self):
         
-        fhir_contents = self.fhir_contents
+        fhir_contents = self.fhir_json
         with allure.step("FHIR contents"):
             self.allure_util.allure_attach_with_text("contents of FHIR report", str(fhir_contents))
         assert fhir_contents is not None, f"Annalise-cxr-FHIR.json does not exist at {self.fhir_output_path} or the contents are None"
         cxr_req = self.cxr_req
-        count = 0
+
         # non_nuance_findings = ["acute_humerus_fracture", "acute_rib_fracture", "acute_clavicle_fracture"]
         try:
             for observation in range(3,len(fhir_contents['contained'])):
@@ -33,34 +33,43 @@ class TestFHIR(BaseClass):
                 with allure.step("Observation Code"):
                     self.allure_util.allure_attach_with_text("Observation Code", str(target_obs))
                 if target_obs == "246501002":
-                    count+=1
+
                     pass
                 
-                elif len(fhir_contents["contained"][observation]["code"]["coding"])==2 and cxr_req[target_obs][0]["Annalise_coding_system"]  and cxr_req[target_obs][0]["Nuance_coding_system"]:
-                    Annalise_code_as_per_req = cxr_req[target_obs][0]["Annalise_observation.code"]
-                    fhir_annalise_obs_code = fhir_contents["contained"][observation]["code"]["coding"][0]["code"]
-                    assert Annalise_code_as_per_req == fhir_annalise_obs_code, f"{Annalise_code_as_per_req} from requrirement and {fhir_annalise_obs_code} from FHIR are not matching"
-                    print(f"Annalise observation code {Annalise_code_as_per_req} from Requirements and {fhir_annalise_obs_code} from FHIR json is matching")
-                    Nuance_code_as_per_req = cxr_req[target_obs][0]["Nuance_observation.code"]
-                    fhir_nuance_obs_code = fhir_contents["contained"][observation]["code"]["coding"][1]["code"]
-                    assert Nuance_code_as_per_req == fhir_nuance_obs_code, f"{Nuance_code_as_per_req} from requrirement and {fhir_nuance_obs_code} from FHIR are not matching"
-                    print(f"Nuance observation code {Nuance_code_as_per_req} from Requirements and {fhir_nuance_obs_code} from FHIR json is matching")
-                    count+=1
-                
-                elif len(fhir_contents["contained"][observation]["code"]["coding"])==1:
-                    if cxr_req[target_obs][0]["RadElement_coding_system"]:
-                        RadElement_code_as_per_req = cxr_req[target_obs][0]["RadElement_observation.code"]
-                        fhir_RadElement_obs_code = (fhir_contents["contained"][observation]["code"]["coding"][0]["code"])
-                        assert RadElement_code_as_per_req == fhir_RadElement_obs_code, "radele code not match"
-                        print(f"RadElement observation code {RadElement_code_as_per_req} from Requirements and {fhir_RadElement_obs_code} from FHIR json is matching")
-                        count+=1
-                        
-                    else:
+                else:
+                    if cxr_req[target_obs][0]["Annalise_coding_system"] and cxr_req[target_obs][0]["Nuance_coding_system"]:
+                        assert len(fhir_contents["contained"][observation]["code"]["coding"])==2, f"More than two Coding systems are displayed in FHIR for {target_obs} observation. Only TWO coding systems are expected as per requirement"
+                        assert cxr_req[target_obs][0]["Annalise_system"] in fhir_contents["contained"][observation]["code"]["coding"][0]["system"],"Annalise coding system text in FHIR does not match with requirement"
+                        assert cxr_req[target_obs][0]["Nuance_system"] in fhir_contents["contained"][observation]["code"]["coding"][1]["system"],"Nuance coding system text in FHIR does not match with requirement"
                         Annalise_code_as_per_req = cxr_req[target_obs][0]["Annalise_observation.code"]
                         fhir_annalise_obs_code = fhir_contents["contained"][observation]["code"]["coding"][0]["code"]
                         assert Annalise_code_as_per_req == fhir_annalise_obs_code, f"{Annalise_code_as_per_req} from requrirement and {fhir_annalise_obs_code} from FHIR are not matching"
                         print(f"Annalise observation code {Annalise_code_as_per_req} from Requirements and {fhir_annalise_obs_code} from FHIR json is matching")
-                        count+=1
+                        Nuance_code_as_per_req = cxr_req[target_obs][0]["Nuance_observation.code"]
+                        fhir_nuance_obs_code = fhir_contents["contained"][observation]["code"]["coding"][1]["code"]
+                        assert Nuance_code_as_per_req == fhir_nuance_obs_code, f"{Nuance_code_as_per_req} from requrirement and {fhir_nuance_obs_code} from FHIR are not matching"
+                        print(f"Nuance observation code {Nuance_code_as_per_req} from Requirements and {fhir_nuance_obs_code} from FHIR json is matching")
+
+                    
+                    elif cxr_req[target_obs][0]["RadElement_coding_system"]:
+                        assert len(fhir_contents["contained"][observation]["code"]["coding"])==1, f"More than one Coding systems are displayed in FHIR for {target_obs} observation. Only one coding system is expected as per requirement"
+                        assert cxr_req[target_obs][0]["RadElement_system"] in fhir_contents["contained"][observation]["code"]["coding"][0]["system"],"RadElement coding system text in FHIR does not match with requirement"
+                        RadElement_code_as_per_req = cxr_req[target_obs][0]["RadElement_observation.code"]
+                        fhir_RadElement_obs_code = (fhir_contents["contained"][observation]["code"]["coding"][0]["code"])
+                        assert RadElement_code_as_per_req == fhir_RadElement_obs_code, "radele code not match"
+                        print(f"RadElement observation code {RadElement_code_as_per_req} from Requirements and {fhir_RadElement_obs_code} from FHIR json is matching")
+
+                    elif cxr_req[target_obs][0]["Annalise_coding_system"]==True and cxr_req[target_obs][0]["Nuance_coding_system"]==False:
+                        assert len(fhir_contents["contained"][observation]["code"]["coding"])==1, f"More than one Coding systems are displayed in FHIR for {target_obs} observation. Only one coding system is expected as per requirement"
+                        assert cxr_req[target_obs][0]["Annalise_system"] in fhir_contents["contained"][observation]["code"]["coding"][0]["system"],"Annalise coding system text in FHIR does not match with requirement"
+                        if "code" in fhir_contents["contained"][observation]["code"]["coding"][0]:
+                            print("SUPPP")
+                        Annalise_code_as_per_req = cxr_req[target_obs][0]["Annalise_observation.code"]
+                        fhir_annalise_obs_code = fhir_contents["contained"][observation]["code"]["coding"][0]["code"]
+                        assert Annalise_code_as_per_req == fhir_annalise_obs_code, f"{Annalise_code_as_per_req} from requrirement and {fhir_annalise_obs_code} from FHIR are not matching"
+                        print(f"Annalise observation code {Annalise_code_as_per_req} from Requirements and {fhir_annalise_obs_code} from FHIR json is matching")
+
+
         except Exception as e:
             print(f"An exception occurred: {e}")
             
@@ -71,44 +80,48 @@ class TestFHIR(BaseClass):
     ''' 
     def test_fhir_obs_display(self):
         print("testing observation Display")
-        fhir_contents = self.fhir_contents
+        fhir_contents = self.fhir_json
         assert fhir_contents is not None, f"Annalise-cxr-FHIR.json does not exist at {self.fhir_output_path} or the contents are None"
         cxr_req = self.cxr_req
         count = 0
         try:
             for observation in range(3,len(fhir_contents['contained'])):
-                target = (fhir_contents["contained"][observation]["code"]["coding"][0]["code"])
-                if target == "246501002":
+                target_obs = (fhir_contents["contained"][observation]["code"]["coding"][0]["code"])
+                if target_obs == "246501002":
                     count+=1
                     pass
-                elif len(fhir_contents["contained"][observation]["code"]["coding"])==2:
-                    Annalise_display_as_per_req = cxr_req[target][0]["Annalise_observation.display"]
-                    fhir_annalise_obs_display = fhir_contents["contained"][observation]["code"]["coding"][0]["display"]
-                    assert Annalise_display_as_per_req == fhir_annalise_obs_display, f"{Annalise_display_as_per_req} from requrirement and {fhir_annalise_obs_display} from FHIR are not matching"
-                    print(f"Annalise observation display {Annalise_display_as_per_req} from Requirements and {fhir_annalise_obs_display} from FHIR json is matching")
-                    Nuance_display_as_per_req = cxr_req[target][0]["Nuance_observation.display"]
-                    fhir_nuance_obs_display = fhir_contents["contained"][observation]["code"]["coding"][1]["display"]
-                    assert Nuance_display_as_per_req == fhir_nuance_obs_display, f"{Nuance_display_as_per_req} from requrirement and {fhir_nuance_obs_display} from FHIR are not matching"
-                    print(f"Nuance observation display {Nuance_display_as_per_req} from Requirements and {fhir_nuance_obs_display} from FHIR json is matching")
-                    count+=1
-
-                elif len(fhir_contents["contained"][observation]["code"]["coding"])==1:
-                    if cxr_req[target][0]["RadElement_coding_system"]:
-                        RadElement_display_as_per_req = cxr_req[target][0]["RadElement_observation.display"]
-                        fhir_RadElement_obs_display = (fhir_contents["contained"][observation]["code"]["coding"][0]["display"])
-                        assert RadElement_display_as_per_req == fhir_RadElement_obs_display, "radele code not match"
-                        print(f"RadElement observation display {RadElement_display_as_per_req} from Requirements and {fhir_RadElement_obs_display} from FHIR json is matching")
-                        count+=1
-                        
-                    else:
-                        Annalise_display_as_per_req = cxr_req[target][0]["Annalise_observation.display"]
+                
+                else:
+                    if cxr_req[target_obs][0]["Annalise_coding_system"] and cxr_req[target_obs][0]["Nuance_coding_system"]:
+                        assert len(fhir_contents["contained"][observation]["code"]["coding"])==2, f"More than two Coding systems are displayed in FHIR for {target_obs} observation. Only TWO coding systems are expected as per requirement"
+                        assert cxr_req[target_obs][0]["Annalise_system"] in fhir_contents["contained"][observation]["code"]["coding"][0]["system"],"Annalise coding system text in FHIR does not match with requirement"
+                        assert cxr_req[target_obs][0]["Nuance_system"] in fhir_contents["contained"][observation]["code"]["coding"][1]["system"],"Nuance coding system text in FHIR does not match with requirement"
+                        Annalise_display_as_per_req = cxr_req[target_obs][0]["Annalise_observation.display"]
                         fhir_annalise_obs_display = fhir_contents["contained"][observation]["code"]["coding"][0]["display"]
                         assert Annalise_display_as_per_req == fhir_annalise_obs_display, f"{Annalise_display_as_per_req} from requrirement and {fhir_annalise_obs_display} from FHIR are not matching"
                         print(f"Annalise observation display {Annalise_display_as_per_req} from Requirements and {fhir_annalise_obs_display} from FHIR json is matching")
-                        count+=1
+                        Nuance_display_as_per_req = cxr_req[target_obs][0]["Nuance_observation.display"]
+                        fhir_nuance_obs_display = fhir_contents["contained"][observation]["code"]["coding"][1]["display"]
+                        assert Nuance_display_as_per_req == fhir_nuance_obs_display, f"{Nuance_display_as_per_req} from requrirement and {fhir_nuance_obs_display} from FHIR are not matching"
+                        print(f"Nuance observation display {Nuance_display_as_per_req} from Requirements and {fhir_nuance_obs_display} from FHIR json is matching")
 
-            print(count)
-            
+                    
+                    elif cxr_req[target_obs][0]["RadElement_coding_system"]:
+                        assert len(fhir_contents["contained"][observation]["code"]["coding"])==1, f"More than one Coding systems are displayed in FHIR for {target_obs} observation. Only one coding system is expected as per requirement"
+                        assert cxr_req[target_obs][0]["RadElement_system"] in fhir_contents["contained"][observation]["code"]["coding"][0]["system"],"RadElement coding system text in FHIR does not match with requirement"
+                        RadElement_display_as_per_req = cxr_req[target_obs][0]["RadElement_observation.display"]
+                        fhir_RadElement_obs_display = (fhir_contents["contained"][observation]["code"]["coding"][0]["display"])
+                        assert RadElement_display_as_per_req == fhir_RadElement_obs_display, "radele code not match"
+                        print(f"RadElement observation display {RadElement_display_as_per_req} from Requirements and {fhir_RadElement_obs_display} from FHIR json is matching")
+                        
+                    elif cxr_req[target_obs][0]["Annalise_coding_system"]==True and cxr_req[target_obs][0]["Nuance_coding_system"]==False:
+                        assert len(fhir_contents["contained"][observation]["code"]["coding"])==1, f"More than one Coding systems are displayed in FHIR for {target_obs} observation. Only one coding system is expected as per requirement"
+                        assert cxr_req[target_obs][0]["Annalise_system"] in fhir_contents["contained"][observation]["code"]["coding"][0]["system"],"Annalise coding system text in FHIR does not match with requirement"
+                        Annalise_display_as_per_req = cxr_req[target_obs][0]["Annalise_observation.display"]
+                        fhir_annalise_obs_display = fhir_contents["contained"][observation]["code"]["coding"][0]["display"]
+                        assert Annalise_display_as_per_req == fhir_annalise_obs_display, f"{Annalise_display_as_per_req} from requrirement and {fhir_annalise_obs_display} from FHIR are not matching"
+                        print(f"Annalise observation display {Annalise_display_as_per_req} from Requirements and {fhir_annalise_obs_display} from FHIR json is matching")
+
         except Exception as e:
             print(f"An exception occurred: {e}")
         
@@ -209,29 +222,29 @@ class TestFHIR(BaseClass):
         except Exception as e:
             print(f"An exception occurred: {e}")
     
-    def test_study_uid(self):
-        print("testing observation Study instance UID")
-        fhir_contents = self.fhir_contents
-        assert fhir_contents is not None, f"Annalise-cxr-FHIR.json does not exist at {self.fhir_output_path} or the contents are None"
-        cxr_req = self.cxr_req
-        count = 0
-        try:
-            study_uid_presence = self.generic_util.is_study_uid_present(fhir_contents)
-            assert study_uid_presence, f"Study Instance UID is not present for one of the observation in FHIR"
-            dicom_study_uid = self.dicom_util.extract_study_uid(self.fhir_input_path)
-            fhir_study_uid = self.generic_util.extract_fhir_study_uid(fhir_contents)
-            assert dicom_study_uid == fhir_study_uid, "Study Instance UID not matching !!"
-        except Exception as e:
-            print(f"An exception occurred: {e}")
-            pytest.fail(f"Test failed: {e}")
+    
+    # def test_study_uid(self):
+    #     print("testing observation Study instance UID")
+    #     fhir_contents = self.fhir_json
+    #     assert fhir_contents is not None, f"Annalise-cxr-FHIR.json does not exist at {self.fhir_output_path} or the contents are None"
+    #     cxr_req = self.cxr_req
+    #     try:
+    #         study_uid_presence = self.generic_util.is_study_uid_present(fhir_contents)
+    #         assert study_uid_presence, f"Study Instance UID is not present for one of the observation in FHIR"
+    #         dicom_study_uid = self.dicom_util.extract_study_uid(self.fhir_input_path)
+    #         fhir_study_uid = self.generic_util.extract_fhir_study_uid(fhir_contents)
+    #         assert dicom_study_uid == fhir_study_uid, "Study Instance UID not matching !!"
+    #     except Exception as e:
+    #         print(f"An exception occurred: {e}")
+    #         pytest.fail(f"Test failed: {e}")
+            
             
             
     def test_fhir_tracking_id(self):
         print("testing FHIR Tracking Identifier")
-        fhir_contents = self.fhir_contents
+        fhir_contents = self.fhir_json
         assert fhir_contents is not None, f"Annalise-cxr-FHIR.json does not exist at {self.fhir_output_path} or the contents are None"
         cxr_req = self.cxr_req
-        count = 0
         try:
             for observation in range(3,len(fhir_contents['contained'])):
                 target = (fhir_contents["contained"][observation]["code"]["coding"][0]["code"])
@@ -253,10 +266,9 @@ class TestFHIR(BaseClass):
     
     def test_fhir_tracking_uid(self):
         print("testing FHIR Tracking Unique Identifier")
-        fhir_contents = self.fhir_contents
+        fhir_contents = self.fhir_json
         assert fhir_contents is not None, f"Annalise-cxr-FHIR.json does not exist at {self.fhir_output_path} or the contents are None"
         cxr_req = self.cxr_req
-        count = 0
         try:
             for observation in range(3,len(fhir_contents['contained'])):
                 target = (fhir_contents["contained"][observation]["code"]["coding"][0]["code"])
