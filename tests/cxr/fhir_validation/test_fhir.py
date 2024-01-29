c717a7794634389b50a663d54647c8acbd29bee3
root <root@ip-10-72-9-201.ap-south-2.compute.internal>
2024-01-29 11:14:57 +0000

    m

--- tests/cxr/fhir_validation/test_fhir.py
+++ tests/cxr/fhir_validation/test_fhir.py
@@ -2,7 +2,6 @@ import pytest
 from marinaalchemist import BaseClass
 import allure
 import re
-from pytest_check import check
 
 
 # @pytest.mark.usefixtures("container_auto")
@@ -54,7 +53,6 @@ class TestFHIR(BaseClass):
                     except AssertionError as e:
                         print(f"Assertion error: {e}")
                         raise AssertionError(f"Test failed due to exception: {e}")
-                        continue
                             
                 elif cxr_req[target_obs][0]["RadElement_coding_system"]:
                     assert len(fhir_contents["contained"][observation]["code"]["coding"])==1, f"More than one Coding systems are displayed in FHIR for {target_obs} observation. Only one coding system is expected as per requirement"
